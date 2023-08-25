a3be945ba33ac154d8364805760b8edf85663005
Ubuntu <ubuntu@ip-172-31-43-144.ec2.internal>
2023-08-25 01:25:02 +0000

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,117 @@
+const logotext = "Mukhtar ABASS-GIWA";
+const meta = {
+    title: "Mukhtar-Portfolio",
+    description: "I'm Mukhtar, a Cloud/DevOps Engineer.",
+};
+
+const introdata = {
+    title: "Hi, I'm Mukhtar ABASS-GIWA",
+    animated: {
+        first: "A Technologist",
+        second: "Visionary",
+        third: "Cloud/DevOps Engineer",
+        fourth: "Cloud Enthusiast",
+        fifth: "Pharmacist",
+        sixth: "Life-long Learner",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Experienced DevOps Engineer: Orchestrating Innovation in Cloud Environments",
+    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "Highly skilled and results-driven DevOps Engineer with about one and a half years of hands-on experience in designing, implementing, and managing robust cloud infrastructures and CI/CD pipelines. Passionate about leveraging automation and best practices to optimize development  workflows and ensure seamless deployments. Seeking entry-level opportunities to contribute my expertise and technical acumen to drive innovation and efficiency in cloud-based environments.",
+};
+const worktimeline = [{
+        jobtitle: "Intern DevOps Engineer",
+        where: "Darey.io",
+        date: "2022 - 2023",
+    },
+];
+
+const skills = [{
+        name: "Linux",
+    },
+    
+    {
+        name: "Python",
+    },
+    
+    {
+        name: "AWS",
+    },
+    
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Jenkins",
+    },
+
+    {
+        name: "Git & Github",
+    },
+
+    {
+        name: "Ansible",
+    },
+
+    {
+        name: "Terraform",
+    },
+
+    {
+        name: "Docker",
+    },
+
+    {
+        name: "Kubernetes",
+    },
+
+    {
+        name: "Github Actions"
+    },
+
+    {
+        name: "ELK Stack",
+    },
+
+    {
+        name: "Prometheus and Grafana"
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mukhtarabassgiwa@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/Babbexx-22?tab=repositories",
+    linkedin: "https://www.linkedin.com/in/mukhtar-abass-giwa",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
